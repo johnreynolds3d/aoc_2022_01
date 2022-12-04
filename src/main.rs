5a8c75df7e8152cc7e5bb01246673d07afe34fe0
John Reynolds <felixinutero@gmail.com>
2022-12-05 07:33:09 +0800

    Much better!

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use std::collections::HashMap;
 use std::error::Error;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
@@ -7,34 +6,26 @@ fn main() -> Result<(), Box<dyn Error>> {
     let f = File::open("input.txt")?;
     let f = BufReader::new(f);
 
-    let mut map = HashMap::new();
-
-    let mut elf: u8 = 1;
-    let mut sum: u32 = 0;
     let mut num: u32;
+    let mut sum: u32 = 0;
+    let mut vec = Vec::new();
 
     for line in f.lines() {
-        if line.as_ref().unwrap().is_empty() {
-            map.insert(elf, sum);
-            elf += 1;
-            sum = 0;
-        } else {
+        if !line.as_ref().unwrap().is_empty() {
             num = line?.trim().parse()?;
             sum += num;
+        } else {
+            vec.push(sum);
+            sum = 0;
         }
     }
-    map.insert(elf, sum);
+    vec.sort();
 
-    let mut max: u32 = 0;
-    let mut win: u8 = 0;
+    let l = vec.len();
+    let top3 = vec[l - 1] + vec[l - 2] + vec[l - 3];
 
-    for (key, value) in &map {
-        if value > &max {
-            max = *value;
-            win = *key;
-        }
-    }
-    println!("And the winner is... elf #{} with {} calories!", win, max);
+    println!("Maximum calories held by a single elf: {}.", vec[l - 1]);
+    println!("Calories held by top 3 elves: {}.", top3);
 
     Ok(())
 }
